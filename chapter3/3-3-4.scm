a355ec0fbfe09b58c016bf8ac1e2931a45c4f50a
liuxiao8114 <liuxiao8114@163.com>
2017-02-20 23:49:28 +0800

    update 3.3.4

--- chapter3/3-3-4.scm
+++ chapter3/3-3-4.scm
@@ -1,11 +1,47 @@
+(define (call-each p)
+  (if (null? p)
+    'done
+    (begin (car p) (call-each (cdr p)))
+  )
+)
+
+(define (make-wire)
+  (let ((signal-value 0) (action-proc '()))
+    (define (set-signal! new-value)
+      (if (not (= signal-value new-value))
+        (begin (set! signal-value new-value)
+          (call-each action-proc)
+          'done
+        )
+      )
+    )
+    (define (accept-action-proc! proc)
+      (set! action-proc (cons proc action-proc))
+      (proc)
+    )
+    (define (dispatch m)
+      (cond ((eq? m 'get-signal) signal-value)
+            ((eq? m 'set-signal!) set-signal!)
+            ((eq? m 'add-action!) accept-action-proc!)
+            (else error "Unknown oeration -- WIRE " m)
+      )
+    )
+    dispatch
+  )
+)
+
 (define (get-signal wire)
-  ())
+  (wire 'get-signal))
 
-(define (set-signal wire) ())
+(define (set-signal wire new-value) ((wire 'set-signal!) new-value))
 
-(define (add-action wire proc) ())
+(define (add-action wire proc) ((wire 'add-action!) proc))
 
-(define (after-delay delay proc) ())
+(define (after-delay delay action)
+  (add-to-agenda! (+ delay (current-time the-agenda)) action the-agenda)
+)
+
+
 
 (define (logical-not s)
   (cond ((= s 1) 0)
